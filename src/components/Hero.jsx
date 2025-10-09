ba8c323deef6137ee15259a5c3f86855cb8cf2a2
Marius5656 <karoliukasn.a@gmail.com>
2025-10-09 20:36:50 +0300

    mygtukas veik

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./hero.css";
 import karkarjieras from "../../public/images./karkarjieras.jpg";
-
 export default function Hero() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="container">
       <div className="quarter ink">
-        <img src={karkarjieras} alt="Klinčių karjeras"></img>
+        <img src={karkarjieras} alt="Klinčių karjeras" />
         <div className="overlay">
           <h1>Klinčių karjeras</h1>
-          <p>aprasymas</p>
-          <button className="btn">Skaityti daugiau</button>
+          <p>Trumpas aprašymas</p>
+          <button className="btn" onClick={() => setShowModal(true)}>
+            Skaityti daugiau
+          </button>
         </div>
       </div>
+
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setShowModal(false)}>
+              &times;
+            </span>
+            <h2>Klinčių karjeras</h2>
+            <p>Čia gali rašyti visą tekstą apie karjerą.</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
